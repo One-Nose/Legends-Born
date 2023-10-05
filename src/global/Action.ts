cdc1b1e47147a55832e4672e8b97b1e2c9738cdb
One Nose <idohaolam@gmail.com>
2023-10-05 19:05:55 +0300

    feat: add cards and actions

---
+++ src/global/Action.ts
@@ -0,0 +1,3 @@
+import Card from './Card'
+
+export default class Action extends Card {}
--- src/global/Character.ts
+++ src/global/Character.ts
@@ -1,13 +1,19 @@
 import { Hex } from 'honeycomb-grid'
+import Action from './Action'
+import { BaseCards } from './cards'
 
 export default class Character {
   hex: Hex
   name: string
   color: 'red' | 'blue'
 
+  action: Action
+
   constructor(name: string, color: 'red' | 'blue', startingHex: Hex) {
     this.name = name
     this.color = color
     this.hex = startingHex
+
+    this.action = BaseCards.Stand
   }
 }
