a57e68801dd677a7e52529f5beb019d4e4364a3e
One Nose <idohaolam@gmail.com>
2023-10-05 22:05:11 +0300

    feat: add visible action card

--- src/renderer/GameScene.ts
+++ src/renderer/GameScene.ts
@@ -1,15 +1,18 @@
-import { Scene } from 'phaser'
+import { GameObjects, Geom, Scene } from 'phaser'
 import Game from './Game'
 import Board from '../global/Board'
 import Character from '../global/Character'
 
 export default class GameScene extends Scene {
+  action!: GameObjects.Image
+
   constructor() {
     super('GameScene')
   }
 
   preload(): void {
     this.load.image('hex', 'assets/hex.png')
+    this.load.image('card', 'assets/card.png')
 
     for (const color of ['red', 'blue'])
       this.load.spritesheet(
@@ -21,6 +24,10 @@ export default class GameScene extends Scene {
 
   create(): void {
     new Game(this)
+    this.action = this.add
+      .image(30, 30, 'card')
+      .setOrigin(0, 0)
+      .setVisible(false)
   }
 
   createBoard(board: Board): void {
@@ -37,5 +44,15 @@ export default class GameScene extends Scene {
       .setOrigin(0.5, 0.6)
       .setScale(3)
       .setFlipX(character.color === 'red')
+      .setInteractive(
+        new Geom.Rectangle(18, 13, 12, 19),
+        Geom.Rectangle.Contains
+      )
+      .on('pointerover', () => {
+        this.action.setVisible(true)
+      })
+      .on('pointerout', () => {
+        this.action.setVisible(false)
+      })
   }
 }
